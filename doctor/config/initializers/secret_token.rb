0a1ad3d3fa2e27282217b1b7d35888332dd9265f
Ralph Su <liasu@ebay.com>
2014-08-12 09:07:11 +0800

    init

---
+++ doctor/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doctor::Application.config.secret_key_base = 'db4dbd2c229712b9518b397ae09021546411fae3f84db80e2f597635e85c4326efb517355db42c8c3e87300c385c15c153be5d812bf0bbf8bb3f90e4775c2604'
