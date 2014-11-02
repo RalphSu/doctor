2e8185002841659eac270001896a05141035bdc2
Su, Ralph <suliangfei@gmail.com>
2014-11-02 22:30:56 +0800

    add brand icon; prepare for work_log adding

--- doctor-auth/app/models/doctor.rb
+++ doctor-auth/app/models/doctor.rb
@@ -1,9 +1,9 @@
 class Doctor < ActiveRecord::Base
 
-	attr_accessible :job_title, :department, :record_status, :overall_rate, :address, :certificate, :name, :address, :phone
+	attr_accessible :job_title, :department, :record_status, :overall_rate, 
+		:address, :certificate, :name, :address, :phone,
+		:add_by, :add_by_addr, :add_by_scan_cert, :reviewed
 
 	has_many :work_logs
-
-
 
 end
--- doctor-auth/app/models/work_log.rb
+++ doctor-auth/app/models/work_log.rb
@@ -1,3 +1,9 @@
 class WorkLog < ActiveRecord::Base
 
+	attr_accessible :hosipital_name, :title, :certificate, :start_date, 
+		:end_date, :rate, :add_by, :add_by_addr, :add_by_scan_cert, :reviewed
+
+	belongs_to :hospitals
+	belongs_to :doctors
+
 end
